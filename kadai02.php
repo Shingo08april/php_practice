060ec1144d38a9f4e648a59e8c241533e9f62575
Zhenwu 0804 <sui.mg69.3ts@gmail.com>
2021-05-29 05:23:44 +0000

    メンタリング３

--- kadai02.php
+++ kadai02.php
@@ -21,5 +21,27 @@ $tech_boost = 'tech';
 $tech_boost .= ' boost';
 echo $tech_boost;
 
+//kadai5
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+// 12月を表示する
+echo $calendar_2018[December];
+//①変数名が2018という数字から始まっている。
+//②2月を囲う後ろが「,」ではなく、「、」になっている。
+//③10月が「""」で囲えていない。
+//④最後の行が一行目と同様に2018という数字から始まっている。
 
 
