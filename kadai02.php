64e0eed0a3b577a8e00118e0e1ce1845cae2833b
Zhenwu 0804 <sui.mg69.3ts@gmail.com>
2021-05-29 04:35:55 +0000

    メンタリング

--- kadai02.php
+++ kadai02.php
@@ -7,4 +7,19 @@ echo $a + $b;
 
 
 //kadai2
-$array_month = ["一月"," 二月", "三月", "四月", ]
+$array_month = ["一月"," 二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"];
+echo $array_month[7];
+
+//kadai3
+$hello = "hello";
+$name = "Shingo";
+$world = "'s World";
+echo $hello . $name . $world;
+
+//kadai4
+$tech_boost = 'tech';
+$tech_boost .= ' boost';
+echo $tech_boost;
+
+
+
