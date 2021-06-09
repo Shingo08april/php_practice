4dd58ba10a428e0d823b8a68fc8fe8abb79b7699
Zhenwu 0804 <sui.mg69.3ts@gmail.com>
2021-06-09 09:37:51 +0000

    4.

--- kadai04.php
+++ kadai04.php
@@ -8,7 +8,7 @@ function sum($double){
 }
 echo sum(100);
 
-//2.$a と $b を仮引数に持ち、　$a と $b　を足した結果を返す関数を作成してください
+//2.$aと$bを仮引数に持ち、$aと$bを足した結果を返す関数を作成してください
 
 function sum($a, $b){
     return $a + $b ;
